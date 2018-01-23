aaeb84ce156358296511a9bfbf41105daaf5a0d5
Stephen Afam-Osemene <stephenafamo@gmail.com>
2018-01-23 12:17:08 +0100

    scaffold payments

--- src/AfricasTaking.php
+++ src/AfricasTaking.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace AfricasTalking;
 
 use GuzzleHttp\Client;
--- src/Payments.php
+++ src/Payments.php
@@ -1,8 +1,28 @@
 <?php
-
 namespace AfricasTalking;
+
+use GuzzleHttp\Client;
 
 class Payments extends Service
 {
+	public function __construct($client, $username, $apiKey)
+	{
+		$baseUrl = "http://payments.africastalking.com/";
+
+		if($username === 'sandbox')
+			$this->baseUrl = "http://payments.sandbox.africastalking.com/";
+
+		$this->username = $username;
+		$this->apiKey = $apiKey;
+
+		$this->client = new Client([
+			'base_uri' => $baseUrl,
+			'headers' => [
+				'apikey' => $this->apiKey,
+				'Content-Type' => 'application/json',
+				'Accept' => 'application/json'
+			]
+		]);
+	}
 	
 }
