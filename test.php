6f3d3aa1547983fa2a205d841872bae17571268f
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-12-15 19:19:18 +0300

    WIP

--- test.php
+++ test.php
@@ -2,8 +2,15 @@
 
 $contents = file_get_contents('./tests/Fixtures.php');
 
+$username = $_SERVER['AFRICASTALKING_USERNAME'] ?? '';
+$api = $_SERVER['AFRICASTALKING_API_KEY'] ?? '';
+$api = $_SERVER['AFRICASTALKING_API_KEY'] ?? '';
+$voicePhoneNumber = $_SERVER['AFRICASTALKING_VOICE_PHONE_NUMBER'] ?? '+254711082489';
+
+$contents = str_replace("username = 'sandbox'","username = '{$username}'",$contents);
+$contents = str_replace("apiKey = ''","apiKey = '{$api}'",$contents);
+$contents = str_replace("voicePhoneNumber = '+254711082489'","voicePhoneNumber = '{$voicePhoneNumber}'",$contents);
+//$contents = str_replace("username = 'sandbox'","username = '{$_SERVER['AFRICASTALKING_USERNAME']}'",$contents);
 var_dump($contents);
-//var_dump($_SERVER['AFRICASTALKING_USERNAME']);
-//var_dump($_SERVER['AFRICASTALKING_API_KEY']);
 
 file_put_contents('./tests/Fixtures.php', $contents);
