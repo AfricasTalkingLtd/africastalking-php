a89be8c0490d6127bfdc3f61ec133373e43888f1
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-12-15 19:38:29 +0300

    WIP

--- test.php
+++ test.php
@@ -4,13 +4,18 @@ $contents = file_get_contents('./tests/Fixtures.php.tpl');
 
 $username = $_SERVER['AFRICASTALKING_USERNAME'] ?? '';
 $api = $_SERVER['AFRICASTALKING_API_KEY'] ?? '';
-$api = $_SERVER['AFRICASTALKING_API_KEY'] ?? '';
 $voicePhoneNumber = $_SERVER['AFRICASTALKING_VOICE_PHONE_NUMBER'] ?? '+254711082489';
+$alphanumeric = $_SERVER['AFRICASTALKING_ALPHANUMERIC'] ?? null;
+$short_code = $_SERVER['AFRICASTALKING_PREMIUM_SHORTCODE'] ?? null;
+$payment_product = $_SERVER['AFRICASTALKING_PAYMENT_PRODUCT'] ?? null;
 
 $contents = str_replace("username = 'sandbox'","username = '{$username}'",$contents);
 $contents = str_replace("apiKey = ''","apiKey = '{$api}'",$contents);
 $contents = str_replace("voicePhoneNumber = '+254711082489'","voicePhoneNumber = '{$voicePhoneNumber}'",$contents);
-//$contents = str_replace("username = 'sandbox'","username = '{$_SERVER['AFRICASTALKING_USERNAME']}'",$contents);
+$contents = str_replace("voicePhoneNumber2 = '+254711082489'","voicePhoneNumber2 = '{$voicePhoneNumber}'",$contents);
+$contents = str_replace("alphanumeric = 'Test'","alphanumeric = '{$alphanumeric}'",$contents);
+$contents = str_replace("shortCode = ''","shortCode = '{$short_code}'",$contents);
+$contents = str_replace("productName = 'Test Product'","productName = '{$payment_product}'",$contents);
 var_dump($contents);
 
 file_put_contents('./tests/Fixtures.php', $contents);
