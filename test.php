d967cc70f9eeb9c7c0c999860aea10809d7a39bc
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-12-15 17:03:58 +0300

    WIP

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+require_once 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 var_dump($_ENV);
