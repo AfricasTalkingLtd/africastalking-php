edd156bc48dfce1d1d2b6cb05f39d301a981cf9d
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2024-03-05 20:04:12 +0300

    Remove token code.
    It is not related to the PR subject

--- src/Token.php
+++ src/Token.php
@@ -10,19 +10,4 @@ class Token extends Service
 		$response = $this->client->post('auth-token/generate', ['body' => $requestData ] );
 		return $this->success($response);
     }
-
-    public function createCheckoutToken($options)
-    {
-        if (!isset($options['phoneNumber'])) {
-            return $this->error('phoneNumber must be provided');
-        }
-
-        $requestData = [
-            'username' => $this->username,
-            'phoneNumber' => $options['phoneNumber']
-        ];
-
-        $response = $this->client->post('checkout/token/create', ['form_params' => $requestData]);
-        return $this->success($response);
-    }
 }
